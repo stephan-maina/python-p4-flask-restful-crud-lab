9b7820d08d37de092b443a40f94848b5a2e80981
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-25 01:31:24 +0300

    Completed Lab...

--- server/models.py
+++ server/models.py
@@ -1,16 +1,8 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_serializer import SerializerMixin
+from app import db
 
-db = SQLAlchemy()
-
-class Plant(db.Model, SerializerMixin):
-    __tablename__ = 'plants'
-
+class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    image = db.Column(db.String)
-    price = db.Column(db.Float)
-    is_in_stock = db.Column(db.Boolean)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(255))
+    done = db.Column(db.Boolean, default=False)
 
-    def __repr__(self):
-        return f'<Plant {self.name} | In Stock: {self.is_in_stock}>'
